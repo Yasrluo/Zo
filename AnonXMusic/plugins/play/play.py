d573932ebd1846dfa7128f84af21cb19b5c74614
Abdulhadi <129096669+coowl@users.noreply.github.com>
2023-12-31 01:49:09 +0400

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -45,7 +45,7 @@ async def check_is_joined(message):
         return False
 
 
-@app.on_message(command(["شغل","تشغيل","ف"])
+@app.on_message(command(["شغل","تشغيل"])
     & filters.group
     & ~BANNED_USERS
 )
@@ -363,7 +363,7 @@ async def play_commnd(
                     _,
                     track_id,
                     user_id,
-                    "ف" if video else "a",
+                    "v" if video else "a",
                     "c" if channel else "g",
                     "f" if fplay else "d",
                 )
@@ -497,7 +497,7 @@ async def play_music(client, CallbackQuery, _):
             _["play_13"],
             reply_markup=InlineKeyboardMarkup(buttons),
         )
-    video = True if mode == "ف" else None
+    video = True if mode == "v" else None
     ffplay = True if fplay == "f" else None
     try:
         await stream(
