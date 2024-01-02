1f12159790139ea8665dd5ee64dcbc071d607ac4
Abdulhadi <129096669+coowl@users.noreply.github.com>
2024-01-02 09:49:02 +0400

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ UPSTREAM_REPO = getenv(
     "https://github.com/coowl/Lizz",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv("GIT_TOKEN", "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")  
+GIT_TOKEN = getenv("GIT_TOKEN", "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")  
 # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/zzsvv")
