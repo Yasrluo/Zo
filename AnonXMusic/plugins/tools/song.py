3079b4d51a8ae6609b3f5d4833ec4758d4dfe974
Salamqq <163534782+Salamqq@users.noreply.github.com>
2024-03-29 19:13:00 +0300

    Update song.py

--- AnonXMusic/plugins/tools/song.py
+++ AnonXMusic/plugins/tools/song.py
@@ -62,7 +62,7 @@ def song(client, message):
         message.reply_audio(
             audio_file,
             caption=rep,
-            performer="@A1DIIU . سُــــتِۅࢪيُــاެتِ •𓋾𖥆",
+            performer="@Faeder_Ch . # | 𝑭𝒂𝒆𝒅𝒆𝒓 |",
             thumb=thumb_name,
             title=title,
             duration=dur,
@@ -70,7 +70,7 @@ def song(client, message):
         m.delete()
     except Exception as e:
         m.edit(
-            f"[Victorious](t.me/A1DIIU) 💕**\n\**خطأ :** {e}"
+            f"[Victorious](t.me/Faeder_Ch) 💕**\n\**خطأ :** {e}"
         )
         print(e)
 
